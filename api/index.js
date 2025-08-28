70abcb75b69332e37c9af6139f2121091559d202
MADNA <madnamv@gmail.com>
2025-08-27 20:52:02 -0400

    vercel 3

--- api/index.js
+++ api/index.js
@@ -1,5 +1,4 @@
-import('../dist/madnolia-web-frontend/server/server.mjs').then(module => module.app);
 export default async (req, res) => {
-  const { app } = await import('../dist/madnolia-web-frontend/server/server.mjs');
+  const { default: app } = await import('../dist/madnolia-web-frontend/server/server.mjs');
   return app(req, res);
 };
