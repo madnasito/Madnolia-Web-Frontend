a03822daf81a8a9b09fc4909dd760c6eb5059047
MADNA <madnamv@gmail.com>
2026-08-23 16:47:38 -0400

    download page and videos

--- src/app/pages/download/download.component.ts
+++ src/app/pages/download/download.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { faDesktop, faMobileScreen } from '@fortawesome/free-solid-svg-icons';
+import { Component, Inject, PLATFORM_ID, OnInit } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { faDesktop, faMobileScreen, faWifi, faHeadset, faGamepad, faDownload } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-download',
@@ -7,12 +8,26 @@ import { faDesktop, faMobileScreen } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './download.component.html',
   styleUrl: './download.component.scss'
 })
-export class DownloadComponent {
+export class DownloadComponent implements OnInit {
   faDesktop = faDesktop;
   faMobileScreen = faMobileScreen;
+  faWifi = faWifi;
+  faHeadset = faHeadset;
+  faGamepad = faGamepad;
+  faDownload = faDownload;
 
-  constructor() {}
+  detectedOS: 'windows' | 'linux' | 'other' = 'windows'; // Por defecto Windows
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      const userAgent = window.navigator.userAgent.toLowerCase();
+      if (userAgent.includes('linux') && !userAgent.includes('android')) {
+        this.detectedOS = 'linux';
+      } else if (userAgent.includes('win')) {
+        this.detectedOS = 'windows';
+      }
+    }
   }
 }
--- src/app/pages/lan-party/lan-party.component.ts
+++ src/app/pages/lan-party/lan-party.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { faNetworkWired, faPlayCircle, faDesktop, faGamepad, faToggleOn, faImage, faCheckCircle, faWifi, faShareAlt, faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import Hls from 'hls.js';
 
 @Component({
   selector: 'app-lan-party',
@@ -7,7 +8,10 @@ import { faNetworkWired, faPlayCircle, faDesktop, faGamepad, faToggleOn, faImage
   templateUrl: './lan-party.component.html',
   styleUrl: './lan-party.component.scss'
 })
-export class LanPartyComponent {
+export class LanPartyComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('lanGamesVideo') lanGamesVideo!: ElementRef<HTMLVideoElement>;
+  private hls: Hls | null = null;
+
   faNetworkWired = faNetworkWired;
   faPlayCircle = faPlayCircle;
   faDesktop = faDesktop;
@@ -18,4 +22,26 @@ export class LanPartyComponent {
   faWifi = faWifi;
   faShareAlt = faShareAlt;
   faArrowRight = faArrowRight;
+
+  ngAfterViewInit() {
+    if (this.lanGamesVideo) {
+      const video = this.lanGamesVideo.nativeElement;
+      const videoSrc = 'public/media/lan_games_hls/master.m3u8';
+
+      if (Hls.isSupported()) {
+        this.hls = new Hls();
+        this.hls.loadSource(videoSrc);
+        this.hls.attachMedia(video);
+      } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
+        // Native support for Safari
+        video.src = videoSrc;
+      }
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.hls) {
+      this.hls.destroy();
+    }
+  }
 }
